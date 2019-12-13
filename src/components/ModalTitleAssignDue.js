7a797396e8c9ecfe7973c45706bd20bc2a69eb72
nenadbarjak <nenadbarjak@gmail.com>
2019-12-13 16:57:24 +0100

    Switched to conditional rendering in ModalTitleAssignDue component.

--- src/components/ModalTitleAssignDue.js
+++ src/components/ModalTitleAssignDue.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import moment from 'moment'
 import DatePicker from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css";
@@ -10,6 +10,9 @@ const ModalTitleAssignDue = ({ card, boardId }) => {
 
   const { boardsDispatch, users } = useContext(BoardContext)
 
+  const [datepickerVisible, setDatepickerVisible] = useState(false)
+  const [usersListVisible, setUsersListVisible] = useState(false)
+
   const user = users.filter((item) => card && item.id === card.assignedTo)[0]
   const usersList = useRef()
 
@@ -18,25 +21,6 @@ const ModalTitleAssignDue = ({ card, boardId }) => {
     textColor = 'red'
   } else if (card && card.dueDate - moment() < 86400000) {
     textColor = 'green'
-  }
-
-  const showDatePicker = () => {
-    document.getElementById('due-date').style.display = 'none'
-    document.getElementById('datepicker').style.display = 'block'
-    document.getElementById('date-input').focus()
-  }
-  const closeDatePicker = () => {
-    document.getElementById('due-date').style.display = 'flex'
-    document.getElementById('datepicker').style.display = 'none'
-  }
-  const showUsersList = () => {
-    document.getElementById('assigned-to').style.display = 'none'
-    document.getElementById('users-list').style.display = 'block'
-    usersList.current.focus()
-  }
-  const closeUsersList = () => {
-    document.getElementById('assigned-to').style.display = 'flex'
-    document.getElementById('users-list').style.display = 'none'
   }
 
   const showUnassignButton = () => {
@@ -115,7 +99,7 @@ const ModalTitleAssignDue = ({ card, boardId }) => {
   const handleUserChange = (e) => {
     sendData({assignedTo: e ? e.value : null})
     
-    closeUsersList()
+    setUsersListVisible(false)
   }
 
   const unAssignUser = (e) => {
@@ -136,80 +120,84 @@ const ModalTitleAssignDue = ({ card, boardId }) => {
       />
 
       <div className="assigned-due-container">
-        <div 
-          id="assigned-to" 
-          className="modal-assigned-to" 
-          onClick={showUsersList} 
-          onMouseEnter={showUnassignButton} 
-          onMouseLeave={hideUnassignButton}
-        >
-          <div className={card.assignedTo ? "avatar" : "avatar-unassigned"}>
+        { usersListVisible ? (
+          <div id="users-list">
+            <Select
+              ref={usersList} 
+              options={users.map((user) => ({value: user.id, label: user.firstName + ' ' + user.lastName}))}
+              onBlur={() => setUsersListVisible(false)}
+              isClearable
+              isSearchable
+              defaultValue={(card.assignedTo) ? ({value: user.id, label: user.firstName + ' ' + user.lastName}) : (undefined)}
+              defaultMenuIsOpen
+              onChange={handleUserChange}
+              autoFocus
+            />
+          </div>
+          ) : (
+          <div 
+            className="modal-assigned-to" 
+            onClick={() => setUsersListVisible(true)} 
+            onMouseEnter={showUnassignButton} 
+            onMouseLeave={hideUnassignButton}
+          >
+            <div className={card.assignedTo ? "avatar" : "avatar-unassigned"}>
+              <div>
+                {card.assignedTo ? user.initials : <i className="far fa-user"></i>}
+              </div>
+            </div>
             <div>
-              {card.assignedTo ? user.initials : <i className="far fa-user"></i>}
+              <span>{card.assignedTo ? `Assigned to: ${user.firstName} ${user.lastName}` : 'Unassigned'}</span>
             </div>
-          </div>
+            {card.assignedTo && 
+              <div 
+                className="unassign-cleardate" 
+                id="unassign-button" 
+                onClick={unAssignUser}
+              >
+                <span className="fas fa-times-circle fa-lg"></span>
+              </div>
+            }
+        </div>
+        )}
+        
+        { datepickerVisible ? (
           <div>
-            <span>{card.assignedTo ? `Assigned to: ${user.firstName} ${user.lastName}` : 'Unassigned'}</span>
+            <DatePicker 
+              selected={card.dueDate}
+              showTimeSelect
+              onChange={(date) => sendData({ dueDate: date.getTime() })}
+              onBlur={() => setDatepickerVisible(false)}
+              autoFocus
+            />
           </div>
-          {card.assignedTo && 
-            <div 
-              className="unassign-cleardate" 
-              id="unassign-button" 
-              onClick={unAssignUser}
-            >
-              <span className="fas fa-times-circle fa-lg"></span>
+        ) : (
+          <div 
+            className="modal-due-date"  
+            onClick={() => setDatepickerVisible(true)} 
+            onMouseEnter={showClearDateButton} 
+            onMouseLeave={hideCLearDateButton}
+          >
+            <div className="calendar-icon">
+              <span className="far fa-calendar"></span>
             </div>
-          }
-        </div>
-
-        <div id="users-list">
-          <Select
-            ref={usersList} 
-            options={users.map((user) => ({value: user.id, label: user.firstName + ' ' + user.lastName}))}
-            onBlur={closeUsersList}
-            isClearable
-            isSearchable
-            defaultValue={(card.assignedTo) ? ({value: user.id, label: user.firstName + ' ' + user.lastName}) : (undefined)}
-            defaultMenuIsOpen
-            onChange={handleUserChange}
-          />
-        </div>
-
-        <div 
-          className="modal-due-date" 
-          id="due-date" 
-          onClick={showDatePicker} 
-          onMouseEnter={showClearDateButton} 
-          onMouseLeave={hideCLearDateButton}
-        >
-          <div className="calendar-icon">
-            <span className="far fa-calendar"></span>
-          </div>
-          <div>
-            <span>Due Date: </span>
-          </div>
-          <div>
-            <span style={{color: `${textColor}`}}> {card.dueDate && moment(card.dueDate).calendar()}</span>
-          </div>
-          {card.dueDate && 
-            <div 
-              className="unassign-cleardate" 
-              id="cleardate-button" 
-              onClick={clearDate}
-            >
-              <span className="fas fa-times-circle fa-lg"></span>
+            <div>
+              <span>Due Date: </span>
             </div>
-          }
-        </div>
-
-        <div id="datepicker">
-          <DatePicker id="date-input"
-            selected={card.dueDate}
-            showTimeSelect
-            onChange={(date) => sendData({ dueDate: date.getTime() })}
-            onBlur={closeDatePicker}
-          />
-        </div>
+            <div>
+              <span style={{color: `${textColor}`}}> {card.dueDate && moment(card.dueDate).calendar()}</span>
+            </div>
+            {card.dueDate && 
+              <div 
+                className="unassign-cleardate" 
+                id="cleardate-button" 
+                onClick={clearDate}
+              >
+                <span className="fas fa-times-circle fa-lg"></span>
+              </div>
+            }
+          </div>
+        )}      
       </div>
     </div>
   );
